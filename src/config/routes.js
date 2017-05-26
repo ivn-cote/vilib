3b22af2d264927673481fc0e1c34adae431ff78a
Ivan Kotov <kotovivan@gmail.com>
2017-05-26 16:03:40 +0200

    cleanup

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import CollectionPage from '../containers/CollectionPage';
-import AnotherPage from '../components/AnotherPage';
 import CreatePage from '../containers/CreatePage';
 import NotFoundPage from '../components/NotFoundPage';
 
 export default (
   <Switch>
     <Route exact path="/" component={CollectionPage} />
-    <Route exact path="/another" component={AnotherPage} />
     <Route exact path="/create" component={CreatePage} />
     <Route component={NotFoundPage} />
   </Switch>
